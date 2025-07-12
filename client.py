932f6ec2c9bcbf7785a731088b4d01d936357653
AnejBradacVranc <mikedrifner@gmail.com>
2025-07-12 23:47:20 +0200

    Added db operation methods

--- client.py
+++ client.py
@@ -9,15 +9,23 @@ dbManager = MongoDBClient()
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    #client.publish("smart-heat", payload="hello from Pi", qos=1, retain=False)
+    client.subscribe("smart-heat/register-device", qos=2)
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-	if(msg.topic == "smart-heat/register-device"):		
-		print(msg.topic+" "+str(msg.payload))
+	try:
+		print(msg.topic)
+		if msg.topic == "smart-heat/register-device":		
+			resp = dbManager.update("firebaseDeviceTokens",{"token": str(msg.payload)}, {"token": str(msg.payload)})			
+			if(resp == None):
+				print("ERROR: Could not save device token to db")
+			else:
+				print("Added token to db", str(msg.payload))
+			print(msg.topic+" "+str(msg.payload))
+	except Exception as e:
+		print("Exception in on_message:", e)
+
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
@@ -25,12 +33,6 @@ mqttc.on_message = on_message
 
 mqttc.connect("192.168.1.148", 1883, 60)
 
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-#mqttc.loop_forever()
 run = True
 while run:
 	try:
@@ -39,18 +41,16 @@ while run:
 		roomTemp = humiditySensor.read_temp()
 		distance = distanceSensor.read_distance()
 		
-		print(f"Furnace temperature: {furnaceTemp} C")
-		print(f"Room temperature: {roomTemp} C")
-		print(f"Room humidity: {humidity}")
-		print(f"Distance: {distance} cm")
-		
+		#print(f"Furnace temperature: {furnaceTemp} C")
+		#print(f"Room temperature: {roomTemp} C")
+		#print(f"Room humidity: {humidity}")
+		#print(f"Distance: {distance} cm")		
 		
 		mqttc.publish("smart-heat/furnace-temp", payload=furnaceTemp, qos=1, retain=False)
 		mqttc.publish("smart-heat/room-temp", payload=roomTemp, qos=1, retain=False)
 		mqttc.publish("smart-heat/room-humidity", payload=humidity, qos=1, retain=False)
 		mqttc.publish("smart-heat/distance", payload=distance, qos=1, retain=False)
 		rc = mqttc.loop(timeout=2.0)
-
 
 		if rc != 0:
 			print("Error occured")
--- db.py
+++ db.py
@@ -1,10 +1,12 @@
 from pymongo import MongoClient
+from pymongo import ReturnDocument
+
 class MongoDBClient: 
 	def __init__(self, uri="mongodb://localhost:27017/", db_name="smartHome"):
 		self.client = MongoClient(uri)
 		self.db = self.client[db_name]
 		
-	def insert(collection_name, item):
+	def insert(self, collection_name, item):
 		try:
 			collection = self.db[collection_name]
 			result = collection.insert_one(item)
@@ -13,4 +15,19 @@ class MongoDBClient:
 		except Exception as e:
 			print("Error inserting document:", e)
 			return None
-
+			
+	def update(self, collection_name, filter_query, item):
+		try:
+			collection = self.db[collection_name]		
+			update={"$set": item}
+			result = collection.find_one_and_update(
+			filter_query,
+			update,
+			upsert=True,                
+			return_document=ReturnDocument.AFTER)
+			return result["_id"]
+			print("updated document id:", result.inserted_id)
+			return result.inserted_id
+		except Exception as e:
+			print("Error inserting document:", e)
+			return None
