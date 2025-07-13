61c392638a32e914e12e042f0d259af40cc68d69
AnejBradacVranc <mikedrifner@gmail.com>
2025-07-13 10:45:44 +0200

    Fixes

--- client.py
+++ client.py
@@ -27,14 +27,11 @@ print("Starting cron service")
 scheduler.start()
 
 
-
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, reason_code, properties):
     print(f"Connected with result code {reason_code}")
     client.subscribe("smart-heat/register-device", qos=2)
 
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
 	print(msg)
 	try:
